558245d8b2ed77acb7274439121957c1bcd533d6
CoffeeBoi <extravaganzatoxic@gmail.com>
2024-02-26 13:49:46 +0800

    removed ;

--- recipeapp/recipeapp/pages/_app.js
+++ recipeapp/recipeapp/pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
     <Header />
-    < Component {...pageProps} />;
+    < Component {...pageProps} />
     <Footer />
     </>
 
