f7290b7c204311d3c8b902a1b9fec86d7a1a249b
schwismiss <jodremojo17@gmail.com>
2024-03-02 00:48:41 +0800

    a

--- recipeapp/recipeapp/pages/home.js
+++ recipeapp/recipeapp/pages/home.js
@@ -8,11 +8,8 @@ function Home() {
     return(
       <>
         <div className="Main">
-              <div className="Loading3" id="Loading3"></div>
-              <div className="Loading2" id="Loading2"></div>
-              <div className="Loading" id="Loading">
-                <img src="images/com-cut-unscreen.gif" alt=""/>
-              </div>
+            
+             
         </div>
       
       </>
