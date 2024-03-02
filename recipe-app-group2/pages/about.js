2b1c1e85c29ede01c02e07638b2c132a63917802
schwismiss <jodremojo17@gmail.com>
2024-03-02 12:53:15 +0800

    style

--- recipe-app-group2/pages/about.js
+++ recipe-app-group2/pages/about.js
@@ -4,18 +4,23 @@ import styles from "@/styles/Home.module.css"; // Ensure correct path to your CS
 function About() {
     return (
         <div className={styles.aboutcontainer}>
+
+          <div className={styles.abovecont}>
+                <img src="static/images/abovebord.jpg" className={styles.img2} alt="Logo" />
+          </div>
+
+          <div className={styles.aboutmaincomp}>
+
+              <div className={styles.leftcont}>
+                  <p>Nice to meet you!!</p>
+              </div>
+
+              <div className={styles.rightcont}>
+                  <img src="static/images/eatdess.jpg" className={styles.img} alt="Logo" />
+              </div>
+          </div>
+
             
-            <div className={styles.leftcont}>
-                <p>Nice to meet you!!</p>
-            </div>
-
-            <div className={styles.rightcont}>
-                <img src="static/images/eatdess.jpg" className={styles.img} alt="Logo" />
-            </div>
-
-            <div className={styles.abovecont}>
-              <img src="static/images/aboveborder.jpg" className={styles.img2} alt="" />
-            </div>
         </div>
     );
 }
--- recipe-app-group2/pages/services.js
+++ recipe-app-group2/pages/services.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import styles from "@/styles/Home.module.css"; // Ensure correct path to your CSS module
+
 function Services() {
     return(
-      <>
-      <div className="container">
+   
+      <div className={styles.servicescontainer}>
         <p>Services</p>
       </div>
-      </>
+    
     )
   }
   export default Services
