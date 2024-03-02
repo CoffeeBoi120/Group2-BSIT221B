845e4214650b7c10255e84bef27ac50011e95575
schwismiss <jodremojo17@gmail.com>
2024-03-02 15:42:10 +0800

    update

--- recipe-app-group2/pages/recipelist/[description].js
+++ recipe-app-group2/pages/recipelist/[description].js
@@ -1,41 +1,66 @@
 import fs from 'fs';
 import path from 'path';
 import Image from "next/image";
+import styles from "@/styles/Home.module.css";
 
 function Recipelist( {recipe} ){
     return(
         <>
-        <div key={recipe.id}>
-            <h1>{recipe.name}</h1>
-            <img
-            src={`/./${recipe.image}`}
-            alt={recipe.name}
-            width={500}
-            height={500}
-            />
-            <h2>{recipe.information}</h2>
-            <h3>{"Dessert Type: " + recipe.class.desserttype  + ", " + " Origin: " + recipe.class.origin}</h3>
-            <h4>{"Servings: " + recipe.servings}</h4>
-            <h5>{"Cooking Time: " + recipe.cookingTime  + ", " + " Prepping Time: " + recipe.preppingTime}</h5>
-            <text className="title">Ingredients:</text>
-            <ul>
-                {recipe.ingredients.map((ingredients, index) => (
-                    <li className="list" key={index}>{ingredients}</li>
-                ))}
-            </ul>
-            <text className="title">Steps:</text>
-            <ol>
-                {recipe.steps.map((steps, index) => (
-                    <li className="list" key={index}>{steps}</li>
-                ))}
-            </ol>
-            <text className="title">Nutrition Facts:</text>
-            <ul>
-                <li className="list">{"Calories: " + recipe.nutrition.calories}</li>
-                <li className="list">{"Fats: " + recipe.nutrition.fat}</li>
-                <li className="list">{"Carbohydrates: " + recipe.nutrition.carbs}</li>
-                <li className="list">{"Protein: "+ recipe.nutrition.protein}</li>
-            </ul>
+        <div className={styles.Maincomp}>
+            <div key={recipe.id}>
+
+                <div className={styles.topContainer}>
+                    <div className={styles.rightContain}>
+                        <img
+                        src={`/./${recipe.image}`}
+                        alt={recipe.name}
+                        width={500}
+                        height={500}
+                        className={styles.recipeImage}
+                        />
+                    </div>
+                    <div className={styles.leftContain}>
+                        <h1 className={styles.recipeName}>{recipe.name}</h1>
+                        <h2 className={styles.recipeInfo}>{recipe.information}</h2>
+                    </div>
+                </div>
+                    
+                <div className={styles.middleContainer}>
+                    <img src="/static/images/upbordermain.png" alt="" />
+                </div >
+                
+                
+
+                <div className={styles.bottomContainer}>
+                    <h3>{"Dessert Type: " + recipe.class.desserttype  + ", " + " Origin: " + recipe.class.origin}</h3>
+                    <h4>{"Servings: " + recipe.servings}</h4>
+                    <h5>{"Cooking Time: " + recipe.cookingTime  + ", " + " Prepping Time: " + recipe.preppingTime}</h5>
+                    <text className={styles.title}>Ingredients:</text>
+                    <ul>
+                        {recipe.ingredients.map((ingredients, index) => (
+                            <li className="list" key={index}>{ingredients}</li>
+                        ))}
+                    </ul>
+                    <text className={styles.title}>Steps:</text>
+                    <ol>
+                        {recipe.steps.map((steps, index) => (
+                            <li className="list" key={index}>{steps}</li>
+                        ))}
+                    </ol>
+                    <text className={styles.title}>Nutrition Facts:</text>
+                    <ul>
+                        <li className="list">{"Calories: " + recipe.nutrition.calories}</li>
+                        <li className="list">{"Fats: " + recipe.nutrition.fat}</li>
+                        <li className="list">{"Carbohydrates: " + recipe.nutrition.carbs}</li>
+                        <li className="list">{"Protein: "+ recipe.nutrition.protein}</li>
+                    </ul>
+                </div>
+
+                <div className={styles.bottomBorder}>
+                    <img src="/static/images/botbordermain.png" alt="" />
+                </div >
+
+            </div>
         </div>
         </>
     )
--- recipe-app-group2/pages/recipelist/index.js
+++ recipe-app-group2/pages/recipelist/index.js
@@ -2,30 +2,46 @@ import Link from 'next/link'
 import fs from 'fs';
 import path from 'path';
 import Image from "next/image";
-import "@/styles/Home.module.css";
+import styles from "@/styles/Home.module.css";
 
 function Recipes( {recipes} ) {
   return(
     <>
-     <h1>Recipes</h1>
-     {
-       recipes.map(recipe => {
-        return (
-         <div key={recipe.id}>
-         <Link href={"/recipelist/" + recipe.id}>
-           <img
-            src={`/./././${recipe.image}`}
-            alt={recipe.name}
-            width={300}
-            height={300}
-            />
-            {recipe.name}
-            </Link>
-          </div>
-        )
-      })
-    }
-      </>
+    <div className={styles.mainp}>
+
+      <div className={styles.mainpagehead}> 
+        <img src="/static/images/mainheader.png" alt="" />
+      </div>
+
+      <div className={styles.secondhead}> 
+        <p>"In the world of desserts, every bite is a chapter in the book of indulgence, where each page unfolds a tale of sweet satisfaction and culinary delight."</p>
+      </div>
+
+      <div className={styles.mainpagecont}> 
+       
+        {
+          recipes.map(recipe => {
+            return (
+              
+            <div key={recipe.id}>
+            <Link href={"/recipelist/" + recipe.id}>
+
+              <img
+                src={`/./././${recipe.image}`}
+                alt={recipe.name}
+                width={300}
+                height={300}
+                />
+                <p className={styles.recipeName}>{recipe.name}</p>
+                </Link>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  
+    </>
     )
   }
   export default Recipes
