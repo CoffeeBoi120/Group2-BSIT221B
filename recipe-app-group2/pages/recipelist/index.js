91752fdb6e78fa21c6d88c6ab7daaf5e1b6a45b7
schwismiss <jodremojo17@gmail.com>
2024-03-02 13:03:40 +0800

    style

--- recipe-app-group2/pages/recipelist/index.js
+++ recipe-app-group2/pages/recipelist/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import fs from 'fs';
 import path from 'path';
 import Image from "next/image";
+import "@/styles/Home.module.css";
 
 function Recipes( {recipes} ) {
   return(
