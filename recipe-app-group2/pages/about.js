83ffbad20072c796c53e545eb84fb9bece5b707a
schwismiss <jodremojo17@gmail.com>
2024-03-03 22:31:17 +0800

    ethan pogi

--- recipe-app-group2/pages/about.js
+++ recipe-app-group2/pages/about.js
@@ -12,14 +12,41 @@ function About() {
 
           <div className={styles.aboutcontainer}>
 
-              <div className={styles.leftcont}>
-                  <p>Nice to meet you!!</p>
-              </div>
+            <div className={styles.about1}>
 
-              <div className={styles.rightcont}>
-                  <img src="static/images/eatcookie.gif" className={styles.img} alt="Logo" />
-              </div>
-              
+                <div className={styles.top}>
+                    <h2>Nice to meet you!!</h2>
+                    <p>Indulge in the sweet symphony of flavors and textures that define our delectable desserts. At our dessert haven, every bite is a tantalizing journey through layers of velvety creams, rich chocolates, and delicate pastries. Our passion for crafting exquisite desserts is matched only by our dedication to sourcing the finest ingredients, ensuring each creation is a masterpiece of taste and artistry.</p>
+                </div>
+
+                <div className={styles.bot}>
+                    <img src="static/images/eatcookie.gif" className={styles.img} alt="Logo" />
+                </div>
+            </div>
+
+            <div className={styles.about2}>
+
+                <div className={styles.rightcont}>
+                    <img src="static/images/aboutpic1.webp" className={styles.img} alt="Logo" />
+                </div>
+
+                <div className={styles.leftcont}>
+                    <p>From the timeless elegance of classic cakes to the innovative twists of modern confections, our dessert repertoire spans the spectrum of culinary delights. Whether you're savoring the comforting warmth of a freshly baked cookie or reveling in the intricate beauty of a meticulously crafted tart, each dessert tells a story of craftsmanship, creativity, and above all, pure indulgence.</p>
+                </div>
+            </div>
+
+            <div className={styles.about3}>
+
+                <div className={styles.rightcont}>
+                    <h2>Nice to meet you!!</h2>
+                    <p>Join us on a culinary adventure where every dessert is a celebration of life's sweet moments. Whether shared with loved ones or savored in solitude, our desserts are more than just treats – they're expressions of joy, comfort, and the simple pleasures that make life deliciously memorable.</p>
+                </div>
+
+                <div className={styles.leftcont}>
+                    <img src="static/images/aboutpic2.png" className={styles.img} alt="Logo" />
+                </div>
+            </div>
+
           </div>
 
 
