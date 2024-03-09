ac101ebfe42465ac300dd9b33aa79069ab53ade7
CoffeeBoi <extravaganzatoxic@gmail.com>
2024-03-09 14:07:50 +0800

    Search fix

--- recipe-app-group2/Components/searchcomponent.js
+++ recipe-app-group2/Components/searchcomponent.js
@@ -1,11 +1,10 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 
-const SearchComponent = ({ recipes }) => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const router = useRouter();
-  
-   
+const SearchComponent = ({ data }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const router = useRouter();
+
   const handleSearch = () => {
     if (searchTerm.trim() === '') {
       // Handle case when search term is empty
@@ -14,30 +13,30 @@ const SearchComponent = ({ recipes }) => {
     }
 
     // Assuming each item has a unique identifier like 'id'
-    const foundRecipe = recipes.find(recipe =>
-      recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+    const foundItem = data.find(item =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
-    if (foundRecipe) {
+    if (foundItem) {
       // Redirect to a specific page or content based on the found item
-      router.push(`/recipelist/${foundRecipe.id}`);
+      router.push(`/recipelist/${foundItem.id}`);
     } else {
       // Handle case when no matching item is found
       console.log('Item not found!');
     }
   };
-  
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button onClick={handleSearch}>Search</button>
-      </div>
-    );
-  };
-  
-  export default SearchComponent;
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchComponent;
