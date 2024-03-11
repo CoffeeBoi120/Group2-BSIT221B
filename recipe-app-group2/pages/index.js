800f8c100ef528f282ca719e75399cb2c638cfb5
schwismiss <jodremojo17@gmail.com>
2024-03-12 02:31:58 +0800

    index imma continue tom morning

--- recipe-app-group2/pages/index.js
+++ recipe-app-group2/pages/index.js
@@ -12,30 +12,24 @@ function Main() {
      <meta name="Landing Page" content="Landing Page of website"></meta>
      </Head>
 
-      <div className="indexcontainer">
+     <div className="indexcontainer">
+      <div className='bg' style={{ backgroundImage: 'url("/static/images/homepagebg.png")' }}>
         <div class="navbar1">
-            <div class="indexlogo">
+          <div class="indexlogo">
             <a href= "/"> <img src="static/images/desertdelightlogo.png" alt="Logo"/> </a>
-            </div>
-            <div class="indexnavmenu">
-                <ul>
-                    <li><a href="/recipelist" class="nav-item" onclick="appearP2()">Recipes</a></li>
-                    <li><a href="/explore" class="nav-item" onclick="Loading()">Explore</a></li>
-                    <li><a href="/about" class="nav-item" onclick="Loading()">About</a></li>
-                </ul>
-            </div>
+          </div>
         </div>
 
         <div class="indexcontent">
-            <div class="homebox">
-                <p>"Indulge in the symphony of sweetness, for in every bite of dessert, we find a delightful harmony of joy and satisfaction."</p>
-                
-                <a href="/recipelist" >
-                  <ButtonExampleAnimated />
-                </a>
-            </div>
+          <div class="homebox">
+            <p>"Indulge in the symphony of sweetness, for in every bite of dessert, we find a delightful harmony of joy and satisfaction."</p>
+            <a href="/recipelist" >
+              <ButtonExampleAnimated />
+            </a>
+          </div>
         </div>
       </div>
+    </div>
 
     
       
