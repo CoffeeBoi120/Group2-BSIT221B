a506d43f4b846c9cf0330c900b2925c63c6c875d
schwismiss <jodremojo17@gmail.com>
2024-03-12 19:30:01 +0800

    ethan cute

--- recipe-app-group2/Components/loading.js
+++ recipe-app-group2/Components/loading.js
@@ -4,7 +4,7 @@ import styles from '../styles/loading.module.scss';
 function Loading() {
   return (
     <div className={styles.Loading1} id="Loading">
-      <img src="/static/images/dessertloading.gif" alt="Loading animation: 2 desserts sliding in frame" />
+      <img src="/static/images/Loadingcake.mp4" alt="Loading animation: 2 desserts sliding in frame" />
     </div>
   );
 }
